7e179e5bee611bd8e93ccb11d7bce5a298a7746b
whichfinder <whichfinder@users.noreply.github.com>
2019-09-26 16:49:37 +0300

    Revert "test"

--- src/locators/main_page_locators.py
+++
@@ -1,5 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-SEARCH = (By.CSS_SELECTOR, 'jss269 jss254')
-
--- src/tests/test_login.py
+++ src/tests/test_login.py
@@ -5,30 +5,27 @@ import time
 from selenium import webdriver
 
 from src.pages.login_page import LoginPage
-from src.pages.main_page import MainPage
 
 
 class LoginTestSuite(unittest.TestCase):
+
     URL = 'https://acme.qc2.blnsearch.net'
 
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(executable_path='/home/okura/PycharmProjects/chromedriver')
         self.driver.get(self.URL)
         self.email = 'admin@acme.com'
         self.password = 'password'
-        self.stext = 'test'
 
     def tearDown(self) -> None:
         self.driver.quit()
 
     def test_login(self):
+        time.sleep(10)
         login_page = LoginPage(self.driver)
+        time.sleep(5)
         login_page.specify_email(self.email)
+        time.sleep(5)
         login_page.specify_pass(self.password)
+        time.sleep(10)
         login_page.press_login()
-        time.sleep(10)
-
-    def test_main_page(self):
-        main_page = MainPage(self.driver)
-        main_page.specify_search(self.stext)
-        time.sleep(10)
