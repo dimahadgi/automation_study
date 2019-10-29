8cc79ceddfd4aa99b608e51241a42518706826fa
Dmytro Khadzhy <dkhad@softserveinc.com>
2019-10-29 17:46:33 +0200

    Added new module - db_helper

---
+++ src/utils/db_postgress_helper.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+query = "select email from worker;"
+
+def postgress_conn(record):
+    try:
+        connection = psycopg2.connect(user="qcmaster",
+                                      password="roo7IaquieTiem",
+                                      host="worker-search-qc.cbqlnvvoalu1.eu-central-1.rds.amazonaws.com",
+                                      port="5432",
+                                      database="qc2_employer")
+        cursor = connection.cursor()
+        cursor.execute(record)
+        record = cursor.fetchall()
+        return record
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+
+if __name__ == "__main__":
+    print(postgress_conn(query))
