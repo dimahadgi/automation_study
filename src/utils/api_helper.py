30ff90dc51bdd58e08294817f971d80c7a42cf62
Dmytro Khadzhy <dkhad@softserveinc.com>
2019-11-25 15:41:26 +0200

    modified certificates generator and api_helper for Prod env

--- src/utils/api_helper.py
+++ src/utils/api_helper.py
@@ -6,9 +6,10 @@ from src.config_parser import Config
 
 class ApiHelper:
     def __init__(self):
-        self.auth_url = os.path.join(Config.api_test_host, Config.api_test_auth_path)
-        self.login = Config.api_test_login
-        self.password = Config.api_test_password
+        self.auth_url = os.path.join(Config.api_host, Config.api_auth_path)
+        self.organization_key = Config.api_organization_key
+        self.login = Config.api_login
+        self.password = Config.api_password
         self.auth_token = self.auth_get_token()
         self.header = {'Authorization': 'bln type=session,version=1,token="{}"'.format(self.auth_token)}
 
@@ -16,7 +17,7 @@ class ApiHelper:
         response = requests.post(self.auth_url,
                                  data={'grantType': "password_persistent",
                                        'identifier': self.login,
-                                       'organizationKey': "acme",
+                                       'organizationKey': self.organization_key,
                                        'password': self.password})
         return response.json().get('token')
 
@@ -27,3 +28,8 @@ class ApiHelper:
     def do_post_request(self, post_url, body):
         response = requests.post(post_url, headers=self.header, json=body)
         return response
+
+
+if __name__ == "__main__":
+    api = ApiHelper()
+    api.auth_get_token()
--- src/utils/certificates_generator.py
+++ src/utils/certificates_generator.py
@@ -11,21 +11,21 @@ from src.utils.db_postgress_helper import DbConnect
 
 class CertificatesGenerator:
     def __init__(self):
-        self.exp_date = "{}-11-05T16:01:38.440Z".format(random.randint(2017, 2030))
+        self.exp_date = "{}-11-05T16:01:38.440Z".format(random.randint(2017, 2023))
         self.image_file = "efb9c5a7-862b-46ca-9ce3-7c8110d0cbff_share rules.png"
-        self.issued_date = "2018-11-05T16:01:38.433Z"
-        self.url = os.path.join(Config.api_test_host, Config.api_test_post_path)
-        self.get_id_url = os.path.join(Config.api_test_host, Config.api_test_get_workers_id_url)
+        self.issued_date = "2017-11-05T16:01:38.433Z"
+        self.url = os.path.join(Config.api_host, Config.api_post_path)
+        self.get_id_url = os.path.join(Config.api_host, Config.api_get_workers_id_url)
         self.fake = Faker('en_CA')
         self.api_helper = ApiHelper()
         self.token = self.api_helper.auth_token
         self.header = {'Authorization': 'bln type=session,version=1,token="{}"'.format(self.token)}
 
-    @staticmethod
-    def get_worker_id_from_db():
-        db_conn = DbConnect()
-        worker_id = db_conn.fetch_all('''select "id" from "worker" where "employerId"=201 and "archived"=false;''')
-        return [x[0] for x in worker_id]
+    # @staticmethod
+    # def get_worker_id_from_db():
+    #     db_conn = DbConnect()
+    #     worker_id = db_conn.fetch_all('''select "id" from "worker" where "employerId"=201 and "archived"=false;''')
+    #     return [x[0] for x in worker_id]
 
     def get_workers_id_from_api(self):
         workers_list = self.api_helper.do_get_request(self.get_id_url).json()
@@ -44,7 +44,7 @@ class CertificatesGenerator:
 
     def add_certificates(self):
         certificates_id = []
-        for workers_id in self.get_worker_id_from_db():
+        for workers_id in self.get_workers_id_from_api():
             response = requests.post(self.url, headers=self.header, json=self.generate_payload(workers_id))
             certificates_id.append(response.json().get('id'))
             if response.status_code == 201:
@@ -58,6 +58,3 @@ class CertificatesGenerator:
 if __name__ == "__main__":
     cert_gen = CertificatesGenerator()
     cert_gen.add_certificates()
-
-
-
