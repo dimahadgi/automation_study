c45e28c69c9ba2491e3cf93b14953bf4e1216a4a
Dmytro Khadzhy <dkhad@softserveinc.com>
2019-11-25 16:00:17 +0200

    get back DB conn code, renamed config variable

--- src/config_parser.py
+++ src/config_parser.py
@@ -8,18 +8,18 @@ class Config:
     file_path = os.path.join(current_dir, '..', "conf.ini")
     config.read(file_path)
 
-    # # Database parameters
-    # db_user = config.get('db_params', 'user')
-    # db_password = config.get('db_params', 'password')
-    # db_host = config.get('db_params', 'host')
-    # db_port = config.get('db_params', 'port')
-    # db_database = config.get('db_params', 'database')
+    # Database parameters
+    db_user = config.get('db_params', 'user')
+    db_password = config.get('db_params', 'password')
+    db_host = config.get('db_params', 'host')
+    db_port = config.get('db_params', 'port')
+    db_database = config.get('db_params', 'database')
 
     # API parameters
     api_login = config.get('api_params', 'login')
     api_password = config.get('api_params', 'password')
     api_host = config.get('api_params', 'host')
     api_auth_path = config.get('api_params', 'auth_path')
-    api_post_path = config.get('api_params', 'post_path')
+    create_certs_path = config.get('api_params', 'create_certs_path')
     api_get_workers_id_url = config.get('api_params', 'get_workers_id_url')
     api_organization_key = config.get('api_params', 'organization_key')
--- src/utils/certificates_generator.py
+++ src/utils/certificates_generator.py
@@ -14,18 +14,18 @@ class CertificatesGenerator:
         self.exp_date = "{}-11-05T16:01:38.440Z".format(random.randint(2017, 2023))
         self.image_file = "efb9c5a7-862b-46ca-9ce3-7c8110d0cbff_share rules.png"
         self.issued_date = "2017-11-05T16:01:38.433Z"
-        self.url = os.path.join(Config.api_host, Config.api_post_path)
+        self.url = os.path.join(Config.api_host, Config.create_certs_path)
         self.get_id_url = os.path.join(Config.api_host, Config.api_get_workers_id_url)
         self.fake = Faker('en_CA')
         self.api_helper = ApiHelper()
         self.token = self.api_helper.auth_token
         self.header = {'Authorization': 'bln type=session,version=1,token="{}"'.format(self.token)}
 
-    # @staticmethod
-    # def get_worker_id_from_db():
-    #     db_conn = DbConnect()
-    #     worker_id = db_conn.fetch_all('''select "id" from "worker" where "employerId"=201 and "archived"=false;''')
-    #     return [x[0] for x in worker_id]
+    @staticmethod
+    def get_worker_id_from_db():
+        db_conn = DbConnect()
+        worker_id = db_conn.fetch_all('''select "id" from "worker" where "employerId"=201 and "archived"=false;''')
+        return [x[0] for x in worker_id]
 
     def get_workers_id_from_api(self):
         workers_list = self.api_helper.do_get_request(self.get_id_url).json()
